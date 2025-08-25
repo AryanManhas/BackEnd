5358296cb769a8e12194fa747b7d580827977357
AarabManhas <2022a1r044@mietjammu.in>
2025-08-25 18:03:59 +0530

    server express

--- 001_basics/express.js
+++ 001_basics/express.js
@@ -1,11 +1,24 @@
-import express from 'express'
+const express = require('express')
 
 // express
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
+//  create the route (after domain xyz.com is domain)
+// xyz.com/ here / route is afte rslash
+
+//app.get(route , requestHandler) //requestHandler is also a middleware
+
+app.get('/', (req, res) => { // requestHandler is a function
+  res.send('Hello User')
+})
+
+
+app.get('/profile', (req, res) => { // requestHandler is a function
   res.send('Hello World')
 })
 
 app.listen(3000)
+
+// nodemon is globally executed to run the file and create server and make the change in realtime eg if we change something on server after reload it will reflect and to run we simpally use tHe command nodemon filename
+
